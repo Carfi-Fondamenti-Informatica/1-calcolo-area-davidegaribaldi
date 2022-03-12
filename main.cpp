9285bd60d09f20b29092a0adbd3f4c3aaa53c66b
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-03-12 17:21:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     float a = 0, b = 0, c = 0,areatriangolo = 0, areaquadrato = 0, arearettangolo = 0, areatrapezio = 0;
     cin>>a>>b>>c;
     areatriangolo = (a * b) / 2;
     areaquadrato=a*a;
     arearettangolo=a*b;
     areatrapezio=(a+b)*c/2;
-    cout<<"L'area del triangolo e' "<<areatriangolo<<endl;
-    cout<<"L'area del quadrato e' "<<areaquadrato<<endl;
-    cout<<"L'area del rettangolo e' "<<arearettangolo<<endl;
-    cout<<"L'area del trapezio e' "<<areatrapezio<<endl;
+    cout<<areatriangolo<<endl;
+    cout<<areaquadrato<<endl;
+    cout<<arearettangolo<<endl;
+    cout<<areatrapezio<<endl;
 
 
     return 0;
