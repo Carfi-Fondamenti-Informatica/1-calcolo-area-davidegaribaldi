1e6b37f786874b4a1d31b18f63260863e4844df0
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-03-12 16:34:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() {
+    float a = 0, b = 0, c = 0, d = 0;
+            float areatriangolo = 0, areaquadrato = 0, arearettangolo = 0, areatrapezio = 0;
+    {
+        cout << "Ciao dammi la base e l'altezza del triangolo " << endl;
+        cin >> a >> b;
+        areatriangolo = (a * b) / 2;
+        cout << "L'area del triangolo e' " << areatriangolo << endl;
+    }
+    {
+        cout << "Ciao dammi il lato del quadrato " << endl;
+        cin >> a;
+        areaquadrato = a * a;
+        cout << "L'area del quadrato e' " << areaquadrato << endl;
+    }
+    {
+        cout << "Ciao dammi la base e l'altezza del rettangolo " << endl;
+        cin >> a >> b;
+        arearettangolo = (a * b);
+        cout << "L'area del rettangolo e' " << arearettangolo << endl;
+    }
+    {
+        cout << "Ciao dammi la base minore,la base maggiore e l'altezza del trapezio " << endl;
+        cin >> a >> b >> c;
+        areatrapezio = (a + b) * c / 2;
+        cout << "L'area del trapezio e' " << areatrapezio << endl;
+    }
 
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    return 0;
 }
